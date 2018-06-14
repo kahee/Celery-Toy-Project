e4987ce38904dcf88dac988851f1a546aa98d840
kahee <yuygh131@gmail.com>
2018-06-14 13:59:19 +0900

    tasks backend 추가

--- tasks.py
+++ tasks.py
@@ -5,10 +5,11 @@ from celery import Celery
 # Celery 인스턴스 생성
 app = Celery(
     'tasks',
-    broker='redis://localhost:6379/0'
+    broker='redis://localhost:6379/0',
+    backend='redis://localhost:6379/0',
 )
 
-# 등록된 작업 처리 
+# 등록된 작업 처리
 @app.task
 def add(x, y):
     time.sleep(5)
