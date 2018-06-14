e646bf6076b46a65f19468f350ccaebff0157764
kahee <yuygh131@gmail.com>
2018-06-14 13:50:20 +0900

    requirements 추가 및 tasks 추가

---
+++ tasks.py
@@ -0,0 +1,15 @@
+import time
+
+from celery import Celery
+
+# Celery 인스턴스 생성
+app = Celery(
+    'tasks',
+    broker='redis://localhost:6379/0'
+)
+
+# 등록된 작업 처리 
+@app.task
+def add(x, y):
+    time.sleep(5)
+    return x + y
