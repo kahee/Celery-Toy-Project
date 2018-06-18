5aced18cb2f3d603df38fe975ff280a73d77c7c3
kahee <yuygh131@gmail.com>
2018-06-18 21:41:44 +0900

    tasks 수정

--- app/books/tasks.py
+++ app/books/tasks.py
@@ -1,11 +1,11 @@
+from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
-from books.models import Book, BookLocation
+from books.models import Book
 from books.utils import get_book_detail
-from config.celery import app
 
 
-@app.task(bind=True)
+@shared_task
 def book_detail_save():
     print(f'{book_id} 상세 정보 크롤')
 
@@ -14,13 +14,13 @@ def book_detail_save():
     for book in books:
         get_book_detail(book.book_id)
         print(f'{book.book_id)}가 저장되었습니다.')
+    return f'끝'
 
-
-@shared_task
-def book_location_save(book_id, location_list):
-    book_location, _ = BookLocation.objects.get_or_create(
-        register_id=location_list[0],
-        location=location_list[1],
-        book_code=location_list[2],
-        book=Book.objects.get(book_id=book_id),
-    )
+# @shared_task
+# def book_location_save(book_id, location_list):
+#     book_location, _ = BookLocation.objects.get_or_create(
+#         register_id=location_list[0],
+#         location=location_list[1],
+#         book_code=location_list[2],
+#         book=Book.objects.get(book_id=book_id),
+#     )
